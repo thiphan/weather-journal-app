5555a01321a2e9da33689be3f401d9b9b2736920
Thi Phan <thoaptk07@gmail.com>
2020-03-26 11:36:06 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Cors for cross origin allowance
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, ()=>{console.log(`Running on localhost: ${port}`)})
+
+//Routes
+app.get('/all', (req,res) =>{
+    res.send(projectData)
+});
+
+app.post('/add', (req,res) => {
+    projectData = {
+        temp: req.body.temp,
+        date: req.body.date,
+        userRes: req.body.userRes
+    };
+    res.send(projectData);
+});
