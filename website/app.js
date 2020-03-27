fa7c4b13ee0c904fa31be2f262cfa7aecf8b07c3
Thi Phan <thoaptk07@gmail.com>
2020-03-26 19:03:37 -0500

    retrieve weather data

--- website/app.js
+++ website/app.js
@@ -1,36 +1,39 @@
 /* Global Variables */
 const apiKey = 'fd9229a49d489baaef55a1b874f315d5';
 
-
+//const feeling = document.getElementById('feeling').value;
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-// const inputElement = document.getElementById('zip').value;
 // console.log(inputElement);
 
 const zip = "38017";
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';//${zip},us&appid=${api_key}`;
 
 //get request to the temperature info API
-//const feeling = document.getElementById('feeling').value;
+//
 
 document.getElementById('generate').addEventListener('click', performAction);
 function performAction(e){
     //const inputZipCode = 38017;
     const inputZipCode =  document.getElementById('zip').value;
+    //const inputUserRes =  document.getElementById('feeling').value;
 
-    console.log(getAnimal(baseURL,inputZipCode, apiKey));
+    getAnimal(baseURL,inputZipCode, apiKey);
+    
     //postData(); with feeling
 }
 
 const getAnimal = async (baseURL,zipCode,key)=>{
-    const res = await fetch(baseURL+zipCode+',us&appid='+key)
+    const res = await fetch(baseURL+zipCode+',us&units=imperial&appid='+key)
     
     try {
         const data = await res.json();
         console.log(data)
-        return data;
+        const requiredData = data.main.temp;
+        console.log(requiredData);
+        
     }  catch(error) {
         console.log("error", error);
         // appropriately handle the error
